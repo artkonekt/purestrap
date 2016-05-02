9312e30aa4bedd944c3f76aaf0266cb9814be3dd
Attila Fulop <fulopattila122@gmail.com>
2016-05-02 17:57:07 +0300

    md docs files converted to html

--- update.js
+++ update.js
@@ -38,6 +38,8 @@ fs.createReadStream(path.join(demoDir, 'purestrap.css')).pipe(fs.createWriteStre
 console.log( "Copied 'purestrap.css'");
 
 var docsDir = path.join(srcDir, 'docs');
+var MarkdownIt = require('markdown-it'),
+	md = new MarkdownIt();
 
 fs.readdir(docsDir, function(err, files) {
         if(err) {
@@ -46,28 +48,24 @@ fs.readdir(docsDir, function(err, files) {
         }
 
         files.forEach( function(file, index) {
-                var srcFile = path.join(docsDir, file);
-                if ('.md' == path.extname(srcFile)) {
-                	fs.stat( srcFile, function( error, stat ) {
-	                    if( error ) {
-	                        console.error( "Error stating file.", error );
-	                        return;
-	                    }
+            var srcFile = path.join(docsDir, file);
+            if ('.md' == path.extname(srcFile)) {
+            	fs.readFile(srcFile, 'utf8', function (err, data) {
+					if (err) {
+						return console.log(err);
+					}
 
-	                    if( stat.isFile() ) {
-	                    	var dstFile = path.join('./docs', file);
-	                    	fs.createReadStream(srcFile).pipe(fs.createWriteStream(dstFile));
-	                        console.log( "Copied '%s' to '%s'.", srcFile, dstFile);
-	                    } else if( stat.isDirectory() ) {
-	                        console.log( "'%s' is a directory.", srcFile );
-	                    }
+					var content = md.render(data);
+					var dstFile = path.join('./docs', file.slice(0, -3));
+					var dstFile = dstFile + '.html';
+					fs.writeFile(dstFile, md.render(data), function(err) {
+					    if(err) {
+					        return console.log(err);
+					    }
 
-                	});
-                }
+					    console.log("%s file was saved.", dstFile);
+					});
+				});
+            }
         });
 });
-
-var MarkdownIt = require('markdown-it'),
-    md = new MarkdownIt();
-var result = md.render('# markdown-it rulezz!');
-console.log(result);
